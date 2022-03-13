86ae5897c8fab171219c5438785f9fe9b6c85b6c
saichetanthalla <36496010+saichetanthalla@users.noreply.github.com>
2022-03-13 15:00:05 -0400

    Add files via upload

---
+++ assignment3_1.py
@@ -0,0 +1,230 @@
+from xmlrpc.server import DocCGIXMLRPCRequestHandler
+
+
+def ex1(hours, wage):
+    # Many companies pay time-and-a-half for any hours worked above 40 in a given week.
+    # Complete this function whose inputs are hours worked (hours) and the hourly rate (wage) to
+    # calculate the total wages for the week.
+    #
+    # BEGIN SOLUTION
+    if hours > 40:
+        return (hours-40)*(wage*1.5)+(40*wage)
+    else:
+        return hours*wage
+    # END SOLUTION
+
+
+def ex2(score):
+    # A certain CS professor gives five-point quizzes that are graded on the scale 5-A, 4-B, 3-C, 2-D, 1-F, 0-F.
+    # Complete this function which accepts a quiz score as an input and uses a decision structure to calculate the corresponding
+    # grade.
+    # BEGIN SOLUTION
+    # dicto={5:'A',4:'B',3:'C',2:'D',1:'F',0:'F'}
+    # return dicto[score]
+    if score <=1:
+        return 'F'
+    elif score == 2:
+        return 'D'
+    elif score == 3:
+        return 'C'
+    elif score == 4:
+        return 'B'
+    else:
+        return 'A'
+
+    # END SOLUTION
+
+
+def ex3(score):
+    # A certain CS professor gives 100-point exams that are graded on the scale 90-100: A, 80-89: B, 70-79: C,
+    # 69-69: D, < 60: F. Complete this function which accepts an exam score as input and uses a decision structure
+    # to calculate the corresponding grade.
+    # BEGIN SOLUTION
+    if score >=90:
+        return 'A'
+    elif score >=80:
+        return 'B'
+    elif score >=70:
+        return 'C'
+    elif score >=60:
+        return 'D'
+    else:
+        return 'F'
+    # END SOLUTION
+
+
+def ex4(credits):
+    # A certain college classifies students according to credits earned. A student with less than 7 credits is a Freshman.
+    # At least 7 credits are required to be a Sophomore, 16  to be a Junior and 26 to be classified as Senior.
+    # Complete this function which calculates the class standing from the number of credits earned.
+    # BEGIN SOLUTION
+
+    if credits < 7:
+        return 'Freshman'
+    elif credits >=26:
+        return 'Senior'
+    elif credits >=16:
+        return 'Junior'
+    else:
+        return 'Sophomore'
+    # END SOLUTION
+
+
+def ex5(limit, speed):
+    # The speeding ticket fine policy in Podunksville is $50 plus $5 for each mph over the limit plus a
+    # penalty of $200 for any speed over 90 mph. Complete this function which accepts a speed limit and a clocked
+    # speed and either return the string `Legal` or the amount of fine, if the speed is illegal.
+    # BEGIN SOLUTION
+
+    if speed < limit:
+        return 'Legal'
+    else:
+        extra_speed = speed - limit
+        fine = (extra_speed * 5)+50
+        if speed > 90:
+            fine+=200
+            return fine
+        return fine
+
+    # END SOLUTION
+
+
+def ex6(input_string):
+    # Complete this function to determine if the input_string is a palindrome. Return True or False
+    # Use square brackets to reverse the input_string! Make sure to lower the input string before testing!
+    # BEGIN SOLUTION
+    input_string=input_string.lower()
+    palindrome=input_string[::-1]
+    if input_string==palindrome:
+        return True
+    else:
+        return False
+
+    # END SOLUTION
+
+
+def ex7(month, day):
+
+    # The year is divided into four season: spring, summer, fall (or autumn) and winter.
+    # While the exact dates that the seasons change vary a little bit from year to
+    # year because of the way that the calender is constructed, we will use the following
+    # dates for this exercise:
+
+    # Season  -- First Day
+    # Spring  -- March 20
+    # Summer  -- June 21
+    # Fall  -- September 22
+    # Winter    -- December 21
+
+    # Complete this function which takes in as inputs a month and day. It should
+    # output the season.
+    # input 1: month -- str
+    # input 2: day -- int
+
+    # output: month -- str (Spring, Summer, Fall, Winter)
+
+    # BEGIN SOLUTION
+    
+    # Month_Dicto = {'January':1,'February':2,'March':3,'April':4,'May':5,'June':6,'July':7,'August':8,'September':9,'October':10,'November':11,'December':12}
+    # # if month_dicto [month] >= 3 and day >
+
+    # if  3 <= Month_Dicto[month] <= 6:
+    #     if (Month_Dicto[month] ==3 and day >= 20) or (Month_Dicto[month] <=6 and day <= 20):
+    #         return 'Spring'
+
+    # elif  6 <= Month_Dicto[month] <= 9:
+    #     if (Month_Dicto[month] ==6 and day >= 21) or (Month_Dicto[month] <=9 and day <= 21):
+    #         return 'Summer'
+    # elif  9 <= Month_Dicto[month] <= 12:
+    #     if (Month_Dicto[month] ==9 and day >= 22) or (Month_Dicto[month] <=12 and day <= 20):
+    #         return 'Fall'
+
+    # else:
+    #     return 'Winter'      
+    
+
+    pass
+
+    # END SOLUTION
+
+
+def ex8(year):
+    # Complete this function to check if year is a leap year
+    # Input: year
+    # Output: True or False (Boolean)
+    pass
+    # BEGIN SOLUTION
+
+    # END SOLUTION
+
+
+def ex9(month, day, year):
+    # Complete this function to check if a data is valid, given month, day, and year.
+    # For example, 5/24/1962 is valid, but 9/31/2000 is not
+    # Inputs: month, day, year
+    # Output: True or False (Boolean)
+    # IMPORTANT: Use the function ex8() to determine if year is leap year
+    pass
+    # BEGIN SOLUTION
+    # if 0 >=month >12:
+    #     return False
+    # if month == 2 and day >
+
+
+    # END SOLUTION
+
+
+def ex10(month, day, year):
+    # Complete this function to calculate the day_number given month, day, and year.
+    # Information: The days of the year are often numbered from 1 through 365 (or 366).
+    # This number can be computed in three steps using int arithmetic:
+    # (a) - day_num = 31 * (month - 1) + day
+    # (b) - if the month is after February subtract (4*(month)+23)//10
+    # (c) - if it's a leap year and after February 29, add 1
+    # Hint: First verify the input date is valid, return False if it is not valid; use is_date_valid
+    # IMPORTANT: use the functions you wrote previous, namely, ex8 and ex9.
+    # Inputs: month, day, year
+    # Output: the day number or False (boolean) if the date is invalid.
+
+    # BEGIN SOLUTION
+    pass
+    # END SOLUTION
+
+
+def ex11(plate):
+
+    # In a particular jurisdiction, older license plates consist of three uppercase
+    # letters followed by three digits. When all of the license plates following
+    # that pattern had been used, the format was changed to four digits followed by
+    # three uppercase letters.
+
+    # Complete this function whose only input is a license plate and its output
+    # is: 1) Older/Valid 2) Newer/Valid 3) Invalid
+    # input: plate (str)
+    # output: 'Older/Valid' or 'Newer/Valid' or 'Invalid'
+    # HINT: Use the comparator operators (>=, <=)!
+
+    # BEGIN SOLUTION
+    pass
+    # END SOLUTION
+
+
+def ex12(date):
+    # A magic date is a date where the day multiplied by the month is equal
+    # to the two digit year. For example, June 10, 1960 is a magic date because
+    # June is the sixth month, and 6 times 10 is 60, which is equal to the two
+    # digit year. Complete this function to determine whether or not a date is
+    # a magic date.
+
+    # input: date (str -- month/day/year) -- e.g., 06/01/2022 -- will have leading zero before month and day
+    # output: True or False (bool)
+    # Hint: use string indexing to extract the month, day, and year from the date string
+
+    # BEGIN SOLUTION
+    date=date.split('/')
+    for i in date:
+        date[i]=int(date[i])
+
+    if date[0]*date[1] == date[2]%100:
+        return True
+    # END SOLUTION
